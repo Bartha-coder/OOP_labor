264a7134be7afcb3548aec69ea3c5dafd6b7eb50
Bartha-coder <bartha.robert.dezso@student.ms.sapientia.ro>
2021-02-23 20:59:31 +0200

    elso hazi

---
+++ labor_1/hazifeladat1/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Bartha Robert Dezso");
+        System.out.println("...");
+        String nev = "Bartha Robert Dezso";
+        for( int i=0; i<nev.length(); i++){
+            System.out.println(nev.charAt(i));
+        }
+
+
+    }
+}
